ac8206801c3afd94711f59a719beda6153d2e9a7
David Watson <david@evolutioncode.uk>
2024-10-12 16:49:24 +0100

    Version 4.5.0.1 (2024101250)

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024101150;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires = 2024041600;    // Moodle 4.5.
+$plugin->version = 2024101250;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires = 2024100100;    // Moodle 4.5.
 $plugin->component = 'format_tiles';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "4.5.0.0";
+$plugin->release = "4.5.0.1";
 $plugin->supported = [405, 405]; // Supported Moodle version range.
