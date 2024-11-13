bd9e20538673cc2c24f1982d3ef407b8f6744adf
David Watson <david@evolutioncode.uk>
2024-11-13 11:43:55 +0000

    Allow tile title containing space

--- lib.php
+++ lib.php
@@ -94,7 +94,7 @@ class format_tiles extends core_courseformat\base {
     public function get_section_name($section) {
         global $PAGE;
         $section = $this->get_section($section);
-        if (trim((string)$section->name) != '') {
+        if ((string)$section->name != '') {
             return format_string($section->name, true, ['context' => context_course::instance($this->courseid)]);
         } else if ($section->section == 0) {
             return $PAGE->user_is_editing() ? self::get_default_section_name($section) : '';
